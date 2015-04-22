8f5aa4e59a5a4190045e53543204ec52acfcdee6
lingrace <glin@college.harvard.edu>
2015-04-22 17:35:32 -0400

    bloom filter typing

--- staticfilter.go
+++ staticfilter.go
@@ -23,24 +23,23 @@ func main() {
 
 // type definition for standard bloom filter
 type Bloom struct{
-    // bset BitSet // will use external library for this
-    //hf hash_function // to generate list of bites values h(0)…h(k). type: 
-    m uint64 // size of bit array
-    k uint64 // number of hash functions
-    n uint64 // predicted number of elements in filter
     eps float64 // false positive error bound
+    n uint64 // predicted number of elements in filter
+    m uint64 // size of bit array
+    //hf hash_function // to generate list of bites values h(0)…h(k). type: 
+    k uint64 // number of hash functions
+    // bset BitSet // will use external library for this
 }
 
 // initialize key values using math!!
 func new (num uint64, err_bound float64) Bloom {
-    new_bloom Bloom
     eps := err_bound
     n := num
-    m := math.Ceil(-1 * (float64(n) * math.Log(eps)) / ((math.Log(2) * math.Log(2))))
+    m := uint64(math.Ceil(-1 * (float64(n) * math.Log(eps)) / ((math.Log(2) * math.Log(2)))))
     //hf := Hash.hash_fun // will use external hash function
-    k := math.Ceil((m / float64(n)) * math.Log(2))
+    k := uint64(math.Ceil((m / float64(n)) * math.Log(2)))
     //bset = BitSet.new(m) // will use external bites implementation
-    return new_bloom
+    return Bloom{eps, n, m, k}
 }
 
 // basic methods. may later include methods for changing epsilon, estimating how many elements are in filter, checking how saturated filter is, etc.

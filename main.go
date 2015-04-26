5f75ef1c9993bb7d19b4529e86bec486138927c0
aszanto9 <aszanto@college.harvard.edu>
2015-04-26 02:25:23 -0400

    trying to pull from joe's big thing with ore

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "StaticFilter"
+import "github.com/aszanto9/Blumo/StaticFilter"
+import "bufio"
+import "os"
+
+import "fmt"
 
 func main() {
-	filter = StaticFilter.NewFilter(.01, 5)
-	lines, err = ReadLines("Dictonaries/tinydict.txt")
-	for i := 0; i < len(lines); i++ {
-		filter.Insert()
+	filter := StaticFilter.NewFilter(349899, .001)
+
+	inserted := InsertLines(filter, "Dictionaries/bigdict.txt")
+	//lines := []string{"aron", "grace", "joe", "joseph", "kai ri"}
+
+	test := fmt.Sprint("Inserted ", inserted, " entries.\n Looking for grace: ", filter.Lookup([]byte("grace")), "\n\nLooking for azazaz: ", filter.Lookup([]byte("azazaz")), "\n\n")
+	fmt.Printf(test)
+}
+
+// referenced http://stackoverflow.com/questions/5884154
+func InsertLines(filter *StaticFilter.Filter, path string) int {
+	file, err := os.Open(path)
+	fmt.Printf("Attempting to open file...\n")
+	if err != nil {
+		fmt.Printf("File open failed.\n")
+		panic(err)
+		return 0
 	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	count := 0
+	for scanner.Scan() {
+		//op := fmt.Sprint("Inserting ", lines[i], "...\n")
+		//fmt.Printf(op)
+		filter.Insert([]byte(scanner.Text()))
+		count++
+	}
+	return count
 }
